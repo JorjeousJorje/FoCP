eefb8d807e6cc13b51c32bd3389807474cd2fb96
JorjeousJorje <g.bakharev@g.nsu.ru>
2021-09-22 19:12:05 +0300

    kaif

--- 4th Task/main.cpp
+++ 4th Task/main.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <sciplot/sciplot.hpp>
 #include <colorconsole/color.hpp>
+
 #include "BesselCalculator.h"
 #include <iomanip>
 #include <numeric>
@@ -7,12 +9,18 @@
 #include <cmath>
 
 
+
 using namespace Utils;
 using namespace std::numbers;
+using namespace sciplot;
+
 
 int main() {
 
 	const auto segmentUnderStudy = GenerateLinspaceWithStep(0.0, 2.0 * pi_v<double>, 0.01);
+
+	std::vector<double> besselValues{};
+	std::vector<double> besselDerivativeValues{};
 
 	for (const auto x : segmentUnderStudy) {
 		const auto bessel_1 = BesselCalculator::CalculateBessel<SimpsonMethod>(x, 1, 1000);
@@ -25,6 +33,28 @@ int main() {
 		} else {
 			std::cout << dye::red("false!") << std::endl;
 		}
+
+		besselValues.push_back(bessel_1);
+		besselDerivativeValues.push_back(bessel_0_derivative);
 	}
+
+	Plot plot;
+
+
+	plot.drawCurve(segmentUnderStudy, besselValues).
+		lineWidth(2).
+		label("Bessel_1 Function");
+
+	plot.drawCurve(segmentUnderStudy, besselDerivativeValues).
+		lineWidth(2).
+		label("Bessel_0 Derivative Function");
+
+	plot.drawCurve(segmentUnderStudy, GenerateZeroArray(segmentUnderStudy.size())).
+		lineWidth(1).
+		lineColor("black").
+		label("");
+
+	plot.size(800, 500);
+	plot.show();
 
 }
